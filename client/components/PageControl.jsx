0d26a736cb855f3a310f4302f1d999d9f36c7f14
Paul Laskowski <pelaskowski@gmail.com>
2015-12-01 00:12:24 -0800

    PageControl fixed to reflect proper span of images on the end page

--- client/components/PageControl.jsx
+++ client/components/PageControl.jsx
@@ -8,13 +8,19 @@ PageControl = React.createClass({
   },
 
   render() {
+
+    let end = this.props.start + this.props.count;
+    if (this.props.start + this.props.count > this.props.length) {
+      end = ths.props.length;
+    }
+    
     return (
       <nav>
         <button id='begin' onClick={this.props.beginClick}>begin</button>
         <button id='prev' onClick={this.props.prevClick}>prev</button>
         <button id='next' onClick={this.props.nextClick}>next</button>
         <button id='end' onClick={this.props.endClick}>end</button>
-        <span> You viewing {this.props.start} to {this.props.start + this.props.count} of {this.props.length} images.</span>
+        <span> You viewing {this.props.start} to {end} of {this.props.length} images.</span>
       </nav>
     );
   }
